fc184a94e768581dc611d4b56cf612d85bf3546f
Serg <ksergie@gmail.com>
2018-09-24 15:31:16 +0300

    add signUpPage test

--- src/main/java/com/ksergie/SignUpPage.java
+++ src/main/java/com/ksergie/SignUpPage.java
@@ -9,11 +9,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-public class SignUpPage1 {
-    private EventFiringWebDriver driver;
-    private static final Logger log = LogManager.getLogger(SignUpPage1.class.getName());
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public SignUpPage1(EventFiringWebDriver driver) {
+public class SignUpPage {
+    private EventFiringWebDriver driver;
+    private static final Logger log = LogManager.getLogger(SignUpPage.class.getName());
+
+    public SignUpPage(EventFiringWebDriver driver) {
         this.driver = driver;
     }
 
@@ -21,6 +25,12 @@ public class SignUpPage1 {
     private By linkLogin = By.xpath("//div[@class='haveAnAccount']/a");
     private By linkTermOfService = By.xpath("//a[text()='Terms of Service']");
     private By linkPrivacyPolicy = By.xpath("//label/a[text()='Privacy Policy']");
+    private By fieldUserName = By.id("nameInput");
+    private By fieldUserEmail = By.id("emailInput");
+    private By checkboxIAccept = By.id("legalCheckbox");
+    private By checkboxEmailCheck = By.id("emailCheckbox");
+    private By buttonContinue = By.id("sendInput");
+    private By fieldPassword = By.id("passwordInput");
 
     private void clickSignUpButton(){
         JavascriptExecutor executor = (JavascriptExecutor)driver;
@@ -63,5 +73,47 @@ public class SignUpPage1 {
         for(String windowHandle: driver.getWindowHandles()){
             driver.switchTo().window(windowHandle);
         }
+    }
+
+    private SignUpPage inputName(String name){
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(fieldUserName)).sendKeys(name);
+        return this;
+    }
+
+    private SignUpPage inputEmail(String email){
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(fieldUserEmail)).sendKeys(email);
+        return this;
+    }
+
+    private SignUpPage tickCheckboxes(){
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(checkboxIAccept)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(checkboxEmailCheck)).click();
+        return this;
+    }
+
+    private String getUser(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMddmmss");
+        Date date = new Date();
+        return "tester647382" + dateFormat.format(date);
+    }
+
+    public void inputNameAndEmail(){
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        clickSignUpButton();
+        String user = getUser();
+        inputName(user);
+        String email = user + "@exitget.com";
+        inputEmail(email);
+        tickCheckboxes();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(buttonContinue)).click();
+        wait.until(ExpectedConditions.titleIs("Choose a password to sign into Exitget"));
+        Assert.assertEquals("Choose a password to sign into Exitget", driver.getTitle(), "We are not on Choose a password page");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(fieldPassword)).sendKeys("20ExitGett_18");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(buttonContinue)).click();
+        wait.until(ExpectedConditions.titleIs("Pricing - Exitget"));
+        Assert.assertEquals("Pricing - Exitget", driver.getTitle(), "We are not on Price page");
     }
 }
--- src/test/java/com/ksergie/SignUpPageTests.java
+++ src/test/java/com/ksergie/SignUpPageTests.java
@@ -2,25 +2,31 @@ package com.ksergie;
 
 import org.testng.annotations.Test;
 
-public class SignUpPage1Tests extends BaseTest{
+public class SignUpPageTests extends BaseTest{
 
-    private SignUpPage1 signUpPage1;
+    private SignUpPage signUpPage1;
 
     @Test(testName = "testLoginLink")
     void testLoginLink(){
-        signUpPage1 = new SignUpPage1(eventDriver);
+        signUpPage1 = new SignUpPage(eventDriver);
         signUpPage1.clickLoginLink();
     }
 
     @Test(testName = "testTermOfServiceLink")
     void testTermOfServiceLink(){
-        signUpPage1 = new SignUpPage1(eventDriver);
+        signUpPage1 = new SignUpPage(eventDriver);
         signUpPage1.clickTermOfServiceLink();
     }
 
     @Test(testName = "testPrivacyPolicyLink")
     void testPrivacyPolicyLink(){
-        signUpPage1 = new SignUpPage1(eventDriver);
+        signUpPage1 = new SignUpPage(eventDriver);
         signUpPage1.clickPrivacyPolicyLink();
+    }
+
+    @Test(testName = "testUserRegistration")
+    void testUserRegistration(){
+        signUpPage1 = new SignUpPage(eventDriver);
+        signUpPage1.inputNameAndEmail();
     }
 }
