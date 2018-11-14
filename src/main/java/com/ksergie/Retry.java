dc3e8b383163775e0bfc6ab27ef6aba5afedbf7a
Serg <ksergie@gmail.com>
2018-11-14 16:46:20 +0200

    New retry

--- src/main/java/com/ksergie/Retry.java
+++ src/main/java/com/ksergie/Retry.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 
 public class Retry implements IRetryAnalyzer {
     private int retryCount = 0;
-    private int maxRetryCount = 2;
+    private int maxRetryCount = 1;
 
     public boolean retry(ITestResult result) {
         if (retryCount < maxRetryCount) {
